307e24570e3fe5b18b5359b24d35b12d6e5c62ef
William Kwong <work.williamkwong@gmail.com>
2022-02-24 11:07:01 -0500

    Additional changes

--- kmeansclustering.m
+++ kmeansclustering.m
@@ -6,20 +6,19 @@ function [ cluster, centr ] = kmeansclustering(mat_file, k )
 %   representing in which cluster the corresponding point lies in
 %   centr: m-by-k matrix of the m-dimensional centroids of the k clusters
 
-input = load(mat_file);
-P = input.guiInput;
-num_beamlets = size(input.Dij,2);
-target_dose = input.targetDose;
-beam_width = input.beamWidth;
-d_beam_indicies = input.beamIndicies;
+%input = load(mat_file);
+P = mat_file;
+%num_beamlets = size(input.Dij,2);
+%target_dose = input.targetDose;
+%beam_width = input.beamWidth;
+%d_beam_indicies = input.beamIndicies;
 
 P = transpose(P);
-numP = size(P,2); % number of points
-dimP = size(P,1); % dimension of points
+numP = size(P,2);
+dimP = size(P,1);
 
 % choose k unique random indices between 1 and size(P,2) (number of points)
 randIdx = randperm(numP,k);
-% initial centroids
 centr = P(:,randIdx);
 
 % init cluster array
@@ -28,42 +27,32 @@ cluster = zeros(1,numP);
 % init previous cluster array clusterPrev (for stopping criterion)
 clusterPrev = cluster;
 
-% for reference: count the iterations
 iterations = 0;
-
-% init stopping criterion
-stop = false; % if stopping criterion met, it changes to true
+stop = false;
 
 while stop == false
     
-    % for each data point 
+    
     for idxP = 1:numP
-        % init distance array dist
+        
         dist = zeros(1,k);
-        % compute distance to each centroid
+        
         for idxC=1:k
             dist(idxC) = norm(P(:,idxP)-centr(:,idxC));
         end
-        % find index of closest centroid (= find the cluster)
+        
         [~, clusterP] = min(dist);
         cluster(idxP) = clusterP;
     end
     
-    % Recompute centroids using current cluster memberships:
-        
-    % init centroid array centr
     centr = zeros(dimP,k);
-    % for every cluster compute new centroid
     for idxC = 1:k
-        % find the points in cluster number idxC and compute row-wise mean
         centr(:,idxC) = mean(P(:,cluster==idxC),2);
     end
     
-    % Checking for stopping criterion: Clusters do not chnage anymore
     if clusterPrev==cluster
         stop = true;
     end
-    % update previous cluster clusterPrev
     clusterPrev = cluster;
     
     iterations = iterations + 1;
