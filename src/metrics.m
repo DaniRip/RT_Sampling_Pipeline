932de869b65cd586a52e621c2ed131cc55f9b8cf
Danielle <daripsman@uwaterloo.ca>
2023-04-04 00:01:04 -0400

    Updated metrics calculations
    Separated D*w calculations for target and OARs. Didn't add any new calculations, just vectorized all the current tumor dose calculation outputs.

--- src/metrics.m
+++ src/metrics.m
@@ -9,37 +9,16 @@ function [avg_deviation, max_overage, voxels_underdosed] = metrics(mat_file, fmo
 
     %average deviation from limit
     fmo_output = transpose(fmo_output);
-    D_vb_w_b = input.Dij*fmo_output;
-    deviation = 0;
-    for i = 1:numel(D_vb_w_b)
-        deviation = deviation + abs((input.targetDose - D_vb_w_b(i)));
-    end
-    avg_deviation = deviation/numel(D_vb_w_b);
+    D_target=input.Dij(input.structVoxels.target,:);
+    D_OAR=input.Dij(input.structVoxels.OAR,:);
+    dv_target = D_target*fmo_output;
+    dv_OAR = D_OAR*fmo_output;
 
-    %max overage (max dose of tumor and max dose of OAR) - D_vb*w_b 
-    %gives total dose given to each voxel, then substract the prescribed (
-    %the right hand side of all constraints which is the input of the FMO model (usually called targetDose and value of 42.4)) 
-    %dosage for each voxel, then take the maximum of that vector
-    D_vb_w_b = input.Dij*fmo_output;
-    max_overage = 0;
-    for i = 1:numel(D_vb_w_b)
-        if (D_vb_w_b(i)-input.targetDose) > max_overage
-            max_overage = D_vb_w_b(i)-input.targetDose; 
-        end
-    end
+    % Average deviation from tumor dose:
+    avg_deviation = mean(abs(dv_target-input.targetDose));
 
-    %# of constraints satisfied - leave it out for now
-    %all the ones that don't satisfy the ___
+    % Maximum deviation from tumor dose:
+    max_overage = max(abs(dv_target-input.targetDose));
 
-    %# of voxels underdosed - how do I calculate this?
-    %Multiply the original matrix with the FMO output, then step through
-    %that result to find any voxels that are below the target dosage
-    %CHANGE THIS TO ONLY COUNT TARGET VOXELS
-    D_vb_w_b = input.Dij*fmo_output;
-    voxels_underdosed = 0;
-    for i = 1:numel(D_vb_w_b)
-        if D_vb_w_b(i) < input.targetDose
-            voxels_underdosed = voxels_underdosed + 1;
-        end
-    end
-end
+    % Number of tumor voxels underdosed:
+    voxels_underdosed = sum(dv_target<input.targetDose+.00001);
