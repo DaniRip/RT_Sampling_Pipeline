f79da6920eb9edda817fcc29ed82b96f46c4136c
Gatik Gola <ggola@uwaterloo.ca>
2023-11-16 10:56:44 -0500

    plot functions and fountain algo

---
+++ src/fountainclustering_v2.m
@@ -0,0 +1,62 @@
+function [target_voxels_cluster, OAR_voxels_cluster, target_cluster_centr, OAR_cluster_centr, runtime] = fountainclustering_v2(mat_file)
+    timerVal = tic;
+    [d_target, d_OAR, ~, ~, ~, ~, ~, ~, ~] = integerdownsample(mat_file, 1);
+    state = false;
+    count = 0;
+    
+    % [{cluster_beamlet: [list_of_voxels]}]
+    target_voxels_cluster = containers.Map;
+    OAR_voxels_cluster = containers.Map;
+    % [{cluster_beamlet: centroid}]
+    target_cluster_centr = containers.Map;
+    OAR_cluster_centr = containers.Map;
+
+    % go through each row, cluster each voxel based off its highest 
+    while count < 2
+        if state == false
+            P = d_target;
+        else
+            P = d_OAR;
+        end
+
+        numP = size(P,2); % beamlets
+        dimP = size(P,1); % voxels
+        
+        % init cluster array
+        cluster = zeros(1,dimP); % each idx (voxel) in cluster belongs to cluster(idx)
+        centr = cell(1, numP); % each value corresponds to {highest dose, voxel index}
+        for i = 1:numel(centr)
+            centr{i} = {0, 0};
+        end
+        
+        for idxP = 1:dimP
+            [~,idx] = max(P(idxP,:)); % find dominant beamlet
+            cluster(idxP) = idx;
+            x = isequal(centr{idx}, {0, 0});
+            if x
+                centr{idx} = {P(idxP, idx), idxP};
+            else
+                existing_val = centr{idx};
+                bool = P(idxP, idx) > existing_val{1};
+                centr{idx} = {bool*P(idxP, idx) + (1-bool)*existing_val{1},...
+                    bool*idx + (1-bool)*existing_val{2}};
+            end
+        end
+        %% Add voxels to each cluster_assignment (target and OAR)
+        unique_clusters = unique(cluster); % set of all unique beamlet indices with highest doses
+        for unique_cluster = unique_clusters
+            if count == 0
+                target_voxels_cluster(num2str(unique_cluster)) = find(cluster == unique_cluster);
+                val = centr{unique_cluster};
+                target_cluster_centr(num2str(unique_cluster)) = val{2};
+            else
+                OAR_voxels_cluster(num2str(unique_cluster)) = find(cluster == unique_cluster);
+                val = centr{unique_cluster};
+                OAR_cluster_centr(num2str(unique_cluster)) = val{2};
+            end
+        end
+        state = true;
+        count = count + 1;
+    end
+    runtime = toc(timerVal);
+end
---
+++ src/plot_centroids.m
@@ -0,0 +1,67 @@
+function plot_centroids(target_or_OAR, cluster, centr, mat_file)
+    [~, ~, ~, ~, voxel_coord_target, voxel_coord_OAR, ~, ~, ~] = integerdownsample(mat_file, 1);
+    if target_or_OAR == "target"
+        coordinates = voxel_coord_target;
+    else
+        coordinates = voxel_coord_OAR;
+    end
+    
+    % Prepare for plotting the centroids
+    centroids_x_beam1 = []; % beamlet idx <= 760
+    centroids_x_beam2 = []; % beamlet idx > 760
+    centroids_y_beam1 = [];
+    centroids_y_beam2 = [];
+    centroids_z_beam1 = [];
+    centroids_z_beam2 = [];
+    
+    % fountain clustering
+    if ~isempty(centr)
+        for key = keys(centr)
+            idx = centr(key{1});
+            % centroid indices
+            centr_x = coordinates(idx, 1);
+            centr_y = coordinates(idx, 2);
+            centr_z = coordinates(idx, 3);
+            % Append centroid coordinates
+            if str2double(key) <= 760
+                centroids_x_beam1 = [centroids_x_beam1; centr_x];
+                centroids_y_beam1 = [centroids_y_beam1; centr_y];
+                centroids_z_beam1 = [centroids_z_beam1; centr_z];
+            else
+                centroids_x_beam2 = [centroids_x_beam2; centr_x];
+                centroids_y_beam2 = [centroids_y_beam2; centr_y];
+                centroids_z_beam2 = [centroids_z_beam2; centr_z];
+            end
+        end
+    % any k-means clustering
+    else
+        for key = keys(cluster)
+            idx = cluster(key{1});
+            % voxels indices
+            voxels_x = coordinates(idx, 1);
+            voxels_y = coordinates(idx, 2);
+            voxels_z = coordinates(idx, 3);
+            % centroid indices
+            centr_x = mean(voxels_x);
+            centr_y = mean(voxels_y);
+            centr_z = mean(voxels_z);
+            
+            % Append centroid coordinates
+            centroids_x_beam1 = [centroids_x_beam1; centr_x];
+            centroids_y_beam1 = [centroids_y_beam1; centr_y];
+            centroids_z_beam1 = [centroids_z_beam1; centr_z];
+        end
+    end
+        
+    % Plot only the centroids
+    scatter3(centroids_x_beam1, centroids_y_beam1, centroids_z_beam1, 100, 'o', 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'black');
+    hold on
+    scatter3(centroids_x_beam2, centroids_y_beam2, centroids_z_beam2, 100, 'o', 'MarkerEdgeColor', 'red', 'MarkerFaceColor', 'red');
+    hold off
+    % zmax = 0;
+    % zlim([-50, zmax]);
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+    title('Plot of Centroids');
+end
